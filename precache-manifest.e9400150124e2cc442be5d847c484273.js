e31fa9880a4abf39ad2ded4276e8c0c1e563cfcb
Alex <alexcorreas1994@gmail.com>
2021-09-20 19:36:44 -0300

    Updates

---
+++ precache-manifest.e9400150124e2cc442be5d847c484273.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f60405ebef8d72f4d977d49bf2cda7",
+    "url": "/alexcorreadasilva27.github.io/index.html"
+  },
+  {
+    "revision": "0f02aff41f7f72ad6321",
+    "url": "/alexcorreadasilva27.github.io/static/css/main.60754059.chunk.css"
+  },
+  {
+    "revision": "561ac782cc294c814d0d",
+    "url": "/alexcorreadasilva27.github.io/static/js/2.7fa568fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/alexcorreadasilva27.github.io/static/js/2.7fa568fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f02aff41f7f72ad6321",
+    "url": "/alexcorreadasilva27.github.io/static/js/main.2720c1da.chunk.js"
+  },
+  {
+    "revision": "408f89c8a60b44c467e4",
+    "url": "/alexcorreadasilva27.github.io/static/js/runtime-main.581c447e.js"
+  },
+  {
+    "revision": "30d0c9ad406bf16ab49d7dabdf906eca",
+    "url": "/alexcorreadasilva27.github.io/static/media/wp7248728-star-wars-dark-side-wallpapers.30d0c9ad.jpg"
+  }
+]);
